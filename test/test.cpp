bd8b977b26721eb74e512e8f18f587a3370233d4
TwixXleb <hackermanspacex@gmail.com>
2024-09-21 15:34:34 +0300

    Fix issue with test_04

--- test/test.cpp
+++ test/test.cpp
@@ -22,5 +22,5 @@ TEST(Troll_03, RandomTest) {
 TEST(Troll_04, LotOfVowelsTest) {
   std::string troll_message_04 = "aaaaaaaaa losharaaaaaa";
   std::string result = troll_defence(troll_message_04);
-  EXPECT_EQ(result, "  lshr");
+  EXPECT_EQ(result, " lshr");
 }
